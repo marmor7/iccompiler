607fcb4a48333c9c0c63670499ce1b345732fbf7
gazith <gazith@479d4e48-6051-0410-89c8-7393bcbe9bb0>
2008-07-22 17:53:41 +0000

    removed classes removed unnecessary printings (flags etc.) changed test directory

--- test/UnitTests - Old/runTest.bat
+++ test/UnitTests - Old/runTest.bat
@@ -1,25 +1,23 @@
 
 setLocal EnableDelayedExpansion 
 
-cd C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\IC
+cd F:\workspace\IC_COMPILER\test\UnitTests\IC
 
 for /f "tokens=* delims= " %%a in ('dir/b') do ( 
-set v=%%a 
 move %%a ..\LIR2\%%a
-cd C:\Users\Amir\WORKSP~1\ICC563~1\classes
-java -cp .;java-cup-v11a.jar;java-cup-v11a-runtime.jar IC.Compiler C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\LIR2\%%a -LC:\Users\Amir\workspace\IC_COMPILER\test\libic.sig
-cd C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\IC
+cd F:\workspace\IC_COMPILER\classes
+java -cp .;java-cup-v11a.jar;java-cup-v11a-runtime.jar IC.Compiler F:\workspace\IC_COMPILER\test\UnitTests\LIR2\%%a -LF:\workspace\IC_COMPILER\test\libic.sig -print-lir
+cd F:\workspace\IC_COMPILER\test\UnitTests\IC
 move ..\LIR2\%%a %%a
 )
 
-cd C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\LIR2
+cd F:\workspace\IC_COMPILER\test\UnitTests\LIR2
 
 for /f "tokens=* delims= " %%a in ('dir/b') do ( 
-set v=%%a
-cd C:\temp\microLir\microLir\
-del C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\OUT2\%%a.out
-microlir.bat "C:\Users\Amir\workspace\IC_COMPILER\test\UnitTests\LIR2\%%a" >> C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\OUT2\%%a.out
-cd C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\LIR2
+cd F:\workspace\microLIR
+del F:\workspace\IC_COMPILER\test\UnitTests\OUT2\%%a.out
+microlir.bat "F:\workspace\IC_COMPILER\test\UnitTests\LIR2\%%a" >> F:\workspace\IC_COMPILER\test\UnitTests\OUT2\%%a.out
+cd F:\workspace\IC_COMPILER\test\UnitTests\LIR2
 )
 
-cd C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\
+cd F:\workspace\IC_COMPILER\test\UnitTests\
