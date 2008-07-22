d2b640869e64a06593f32293299edad538e566bb
gazith <gazith@479d4e48-6051-0410-89c8-7393bcbe9bb0>
2008-07-22 17:53:41 +0000

    removed classes removed unnecessary printings (flags etc.) changed test directory

--- trunk/test/UnitTests - Old/runTheirLir.bat
+++ trunk/test/UnitTests - Old/runTheirLir.bat
@@ -1,13 +1,12 @@
 
 setLocal EnableDelayedExpansion 
 
-cd C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\LIR1
+cd F:\workspace\IC_COMPILER\test\UnitTests\LIR1
 
 for /f "tokens=* delims= " %%a in ('dir/b') do ( 
-set v=%%a
-cd C:\temp\microLir\microLir\
-microlir.bat "C:\Users\Amir\workspace\IC_COMPILER\test\UnitTests\LIR1\%%a" >> C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\OUT1\%%a.out
-cd C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\LIR1
+cd F:\workspace\microLIR
+microlir.bat "F:\workspace\IC_COMPILER\test\UnitTests\LIR1\%%a" >> F:\workspace\IC_COMPILER\test\UnitTests\OUT1\%%a.out
+cd F:\workspace\IC_COMPILER\test\UnitTests\LIR1
 )
 
-cd C:\Users\Amir\WORKSP~1\ICC563~1\test\UnitTests\
+cd F:\workspace\IC_COMPILER\test\UnitTests\
